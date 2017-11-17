a849186a3621037bf2a46602e39613390351e135
karnpapon <karnpapon@gmail.com>
2017-11-17 15:26:01 +0700

    update: add grid and group

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      textLength: 0
+      tlength: 0
     }
   }
 
   getTextLen = () => {
+    
     var txt = document.getElementsByTagName("text");
     var txtlen = txt[0].textLength.baseVal.value
     return txtlen
@@ -20,33 +21,48 @@ class App extends Component {
 
   
   runeDraw = (txt) => {
-   console.log(txt)
     var r = new Rune({
       container: "#canvas",
       width: 500,
-      height: 500
+      height: 500,
+      debug: true
     });
+    
+    var fontSze = 6;
+    var xPos = 0;
+    var yPos = 0;
+    
 
     var backgroundColor = r.rect(0, 0, r.width, r.height)
-      .fill(200)
-      .stroke(false)
+    .fill(200)
+    .stroke(false)
 
+    var texttest = r.text("testinput", xPos, yPos, group)
+                  .fontSize(fontSze).stroke(false)
+                  .letterSpacing(false)
 
-    var texttest = r.text("testinput", 20, 300)
-                .fontSize(50).stroke(false)
-                .letterSpacing(false)
-             
-    var xPos = 20;
-    var yPos = 300;
+   
+            
     var txtWdth = txt;
-    var txtHght = 50;
+    var txtHght = txt/4;
+    var group = r.group(50,50)
     
-    var path = r.path(xPos, yPos)
+    var path = r.path(xPos, yPos, group)
     .lineTo(txtWdth, 0)
     .lineTo(txtWdth, txtHght)
     .lineTo(0, txtHght)
     .closePath()
     .stroke(false)
+
+    var grid = r.grid({
+      x: 50,
+      y: 50,
+      width: r.width - 100,
+      height: r.height - 100,
+      gutter: 8,
+      columns: 10,
+      rows: 10
+    });
                 
                 
     r.draw();
@@ -54,17 +70,18 @@ class App extends Component {
 
   updateState = (txt) => {
     this.setState(() => {
-      return { textLength: txt }
+      return { tlength: txt }
     })
   }
 
   componentDidMount = () => {  
     
-    this.runeDraw();
+    this.runeDraw(txtLength);
     
     // stored return values(text lenght) from function
     var txtLength = this.getTextLen();
     this.runeDraw(txtLength);
+    console.log(txtLength)
     
     // this.updateState(txtLength)
     
@@ -72,11 +89,12 @@ class App extends Component {
 
   // update rune onClick
   /*componentWillUpdate = () => {
-    this.runeDraw(this.state.textLength);
+    this.runeDraw(this.state.textsLength);
   }*/
   
 
   render() {
+    const {textsLength} = this.state
     return (
       <div id="canvas">
         {/* <button onClick={this.updateState}>Click ME</button> */}
