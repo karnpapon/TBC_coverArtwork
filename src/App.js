a92f69ef5c344108051b69b296926e9aff0d1bc8
karnpapon <karnpapon@gmail.com>
2017-11-18 15:10:58 +0700

    update: separated function for Rune

--- src/App.js
+++ src/App.js
@@ -8,96 +8,140 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tlength: 0
+      tlength: [],
     }
   }
 
-  getTextLen = () => {
-    
-    var txt = document.getElementsByTagName("text");
-    var txtlen = txt[0].textLength.baseVal.value
-    return txtlen
-  }
 
-  
-  runeDraw = (txt) => {
+  runeInit = () => {
     var r = new Rune({
       container: "#canvas",
       width: 500,
       height: 500,
       debug: true
     });
-    
-    var fontSze = 6;
+    return r
+  }
+
+  runeSet = (r) => {
+
+
+    console.log(r)
+
+    var fontSze = 16;
     var xPos = 0;
     var yPos = 0;
+  
     
-
     var backgroundColor = r.rect(0, 0, r.width, r.height)
-    .fill(200)
+    .fill(240)
     .stroke(false)
+
+    var group = r.group(50, 50)
 
     var texttest = r.text("testinput", xPos, yPos, group)
-                  .fontSize(fontSze).stroke(false)
-                  .letterSpacing(false)
+      .fontSize(fontSze).stroke(false)
+      .letterSpacing(false)
 
-   
-            
-    var txtWdth = txt;
-    var txtHght = txt/4;
-    var group = r.group(50,50)
+    var texttest2 = r.text("line2", xPos, yPos + 20, group)
+      .fontSize(fontSze).stroke(false)
+      .letterSpacing(false)
+
+    var texttest3 = r.text("line3", xPos, yPos + 40, group)
+      .fontSize(fontSze).stroke(false)
+      .letterSpacing(false)
+
+
+    var txt = document.getElementsByTagName("text");
     
-    var path = r.path(xPos, yPos, group)
-    .lineTo(txtWdth, 0)
-    .lineTo(txtWdth, txtHght)
-    .lineTo(0, txtHght)
-    .closePath()
-    .stroke(false)
+    const tlength =  this.state;
+    
+    if(txt){
+      this.setState(() => {
+        var txtlenAll = [];
+        var txtlen1 = txt[0].textLength.baseVal.value
+        var txtlen2 = txt[1].textLength.baseVal.value
+        var txtlen3 = txt[2].textLength.baseVal.value
+        txtlenAll = [txtlen1, txtlen2, txtlen3]
+        this.state.tlength = txtlenAll
+        return this.state.tlength
+        console.log(this.state.tlength);
+      })
+      console.log(this.state.tlength);
+    }
+    
+    console.log(this.state.tlength)
+
+
+    var txtWdth = 40;
+    var txtWdth2 = 40;
+    var txtWdth3 = 40;
+    var txtHght = 40 / 4;
+
+    var path1 = r.path(xPos, yPos, group)
+      .lineTo(txtWdth, 0)
+      .lineTo(txtWdth, txtHght)
+      .lineTo(0, txtHght)
+      .closePath()
+      .stroke(false)
+
+
+    var path2 = r.path(xPos, yPos + 20, group)
+      .lineTo(txtWdth2, 0)
+      .lineTo(txtWdth2, txtHght)
+      .lineTo(0, txtHght)
+      .closePath()
+      .stroke(false)
+
+    var path3 = r.path(xPos, yPos + 40, group)
+      .lineTo(txtWdth3, 0)
+      .lineTo(txtWdth3, txtHght)
+      .lineTo(0, txtHght)
+      .closePath()
+      .stroke(false)
+
+
+    var gridX = 50;
+    var gridY = 50;
 
     var grid = r.grid({
-      x: 50,
-      y: 50,
-      width: r.width - 100,
-      height: r.height - 100,
+      x: gridX,
+      y: gridY,
+      width: r.width - gridX * 2,
+      height: r.height - gridY * 2,
       gutter: 8,
       columns: 10,
       rows: 10
     });
-                
-                
+
+  }
+    
+
+  runeDraw = (r) => {
     r.draw();
   }
 
-  updateState = (txt) => {
-    this.setState(() => {
-      return { tlength: txt }
-    })
+  componentWillMount(){
+   console.log("compponent will mount test log") 
   }
+  componentDidMount(){
+    
+    var r = this.runeInit();
+    this.runeSet(r);
+    this.runeDraw(r);
+    
 
-  componentDidMount = () => {  
-    
-    this.runeDraw(txtLength);
-    
     // stored return values(text lenght) from function
-    var txtLength = this.getTextLen();
-    this.runeDraw(txtLength);
-    console.log(txtLength)
-    
-    // this.updateState(txtLength)
-    
+    // var txtLength = this.getTextLen();
+    // this.runeDraw(txtLength);
   }
 
-  // update rune onClick
-  /*componentWillUpdate = () => {
-    this.runeDraw(this.state.textsLength);
-  }*/
   
 
   render() {
-    const {textsLength} = this.state
+    const {tlength} = this.state;
     return (
       <div id="canvas">
-        {/* <button onClick={this.updateState}>Click ME</button> */}
       </div>
     );
   }
